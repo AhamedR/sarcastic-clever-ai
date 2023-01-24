324d8855c00bd782a3dd849f379dd2373b1c4c41
Ahamed Rasheed <ahamed.rasheed@akqa.com>
2023-01-24 16:21:35 +0530

    feat: added google analytics

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script'
 import styles from '../styles/Home.module.css'
 import MessageBox from '../components/MessageBox'
 
@@ -11,10 +12,24 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto+Serif:opsz,wght@8..144,200&display=swap" rel="stylesheet" />
       </Head>
       <main className={styles.main}>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-935819WG9X"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {
+            `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments)}
+            gtag('js', new Date());
+            gtag('config', 'G-935819WG9X');
+          `
+          }
+        </Script>
         <div className={styles.center}>
           <h2>Sarcastic clever AI</h2>
           <p className={styles.credit}>by <a target='_blank' href='https://ahamedr.vercel.app/' rel="noreferrer">Ahamed</a></p>
